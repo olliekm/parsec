0fea9fdf5fceeb321ede98fbbb29d6d6d85c3797
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-12-07 19:40:54 -0500

    Added search functionality

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,13 +1,22 @@
 import nextra from 'nextra'
 
-const withNextra = nextra({})
+const withNextra = nextra({
+  search: {
+    codeblocks: true,
+  },
+  defaultShowCopyCode: true
+})
 
 export default withNextra({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   turbopack: {
     resolveAlias: {
       'next-mdx-import-source-file': './mdx-components.jsx'
     }
   }
-  
+
 })
